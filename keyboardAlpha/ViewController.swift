f3e4890dd05a902ae8552a5c331afe7a390c97af
Apple Inc <zzc@zzcs-macbook-pro.local>
2018-10-21 23:28:26 -0700

    almost done

--- keyboardAlpha/ViewController.swift
+++ keyboardAlpha/ViewController.swift
@@ -11,12 +11,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var keypad01: UILabel!
     var keypad01Model = keypad()
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        controlPad.layer.borderWidth = constants.keypadBorderWidth
+        QWEPad.layer.borderWidth = constants.keypadBorderWidth
+        RTYPad.layer.borderWidth = constants.keypadBorderWidth
+        UIOPad.layer.borderWidth = constants.keypadBorderWidth
     }
     
     override func didReceiveMemoryWarning() {
@@ -27,9 +28,11 @@ class ViewController: UIViewController {
         if let key = keypad01Model.getKey(with: .Middle) {
             inputField.text.append(key.rawValue)
         }
+        updateBorderColor()
     }
     
     @IBAction func keypad01GestureHandler(_ sender: UIPanGestureRecognizer) {
+        
         if sender.state == .ended {
             if let key = keypad01Model.getKey(with: keypad01Model.direct(of: sender.translation(in: keypad01))) {
                 switch key {
@@ -46,11 +49,52 @@ class ViewController: UIViewController {
                     inputField.text.append(key.rawValue)
                 }
             }
-            
-            //            DisplayLabel.text = "\(keypad01Model.direct(of: sender.translation(in: self.view)))"
+            updateBorderColor()
         }
+    }
+    
+    @IBAction func ClearButtonPressed(_ sender: UIButton) {
+        inputField.text.removeAll()
     }
     
     
 }
 
+
+extension ViewController {
+    private struct constants {
+        static let keypadBorderWidth = 3 as CGFloat
+        static let keypadBorderColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1) as CGColor
+        static let keypadBorderInactiveColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) as CGColor
+    }
+    
+    func updateBorderColor() -> Void {
+        switch keypad01Model.currentState {
+        case .selectKeypad:
+            QWEPad.layer.borderColor = constants.keypadBorderInactiveColor
+            RTYPad.layer.borderColor = constants.keypadBorderInactiveColor
+            UIOPad.layer.borderColor = constants.keypadBorderInactiveColor
+            controlPad.layer.borderColor = constants.keypadBorderInactiveColor
+        case .selectedQWE:
+            QWEPad.layer.borderColor = constants.keypadBorderColor
+            RTYPad.layer.borderColor = constants.keypadBorderInactiveColor
+            UIOPad.layer.borderColor = constants.keypadBorderInactiveColor
+            controlPad.layer.borderColor = constants.keypadBorderInactiveColor
+        case .selectedRTY:
+            QWEPad.layer.borderColor = constants.keypadBorderInactiveColor
+            RTYPad.layer.borderColor = constants.keypadBorderColor
+            UIOPad.layer.borderColor = constants.keypadBorderInactiveColor
+            controlPad.layer.borderColor = constants.keypadBorderInactiveColor
+        case .selectedUIO:
+            QWEPad.layer.borderColor = constants.keypadBorderInactiveColor
+            RTYPad.layer.borderColor = constants.keypadBorderInactiveColor
+            UIOPad.layer.borderColor = constants.keypadBorderColor
+            controlPad.layer.borderColor = constants.keypadBorderInactiveColor
+        case .selectedControlPad:
+            QWEPad.layer.borderColor = constants.keypadBorderInactiveColor
+            RTYPad.layer.borderColor = constants.keypadBorderInactiveColor
+            UIOPad.layer.borderColor = constants.keypadBorderInactiveColor
+            controlPad.layer.borderColor = constants.keypadBorderColor
+        }
+    }
+}
