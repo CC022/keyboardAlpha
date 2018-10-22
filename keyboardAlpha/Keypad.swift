68146cbba54071cb0e57235640a053f206e3a170
Apple Inc <zzc@zzcs-macbook-pro.local>
2018-10-21 22:57:07 -0700

    logic working

--- keyboardAlpha/Keypad.swift
+++ keyboardAlpha/Keypad.swift
@@ -1,18 +1,25 @@
-//
-//  Keypad.swift
-//  keyboardAlpha
-//
-//  Created by zzc on 9/16/18.
-//  Copyright © 2018 zzc. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
 class keypad {
     private struct constants {
-        static let farThreshold:CGFloat = 200
+        //        static let farThreshold:CGFloat = 200
     }
+    
+    enum keys: String {
+        case delete,space,capsLock,enter,q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,Q,W,E,R,T,Y,U,I,O,P,A,S,D,F,G,H,J,K,L,Z,X,C,V,B,N,M
+    }
+    
+    enum keypadState {
+        case selectKeypad
+        case selectedQWE
+        case selectedRTY
+        case selectedUIO
+        case selectedControlPad
+    }
+    
+    var currentState = keypadState.selectKeypad
+    var capLocked = false
     
     enum sweepDirection {
         case UpperLeft
@@ -24,8 +31,6 @@ class keypad {
         case LowerLeft
         case Down
         case LowerRight
-        case FarLeft
-        case FarRight
     }
     
     func direct(of displacement:CGPoint) -> sweepDirection {
@@ -33,14 +38,14 @@ class keypad {
         let angleOfTranslation = atan2f(Float(displacement.x), Float(displacement.y))
         let distOfTranslation = dist(from: displacement)
         
-//        Tap should pass in (0,0) as displacement
+        //        Tap should pass in (0,0) as displacement
         if distOfTranslation == 0 {return .Middle}
         
         switch angleOfTranslation {
         case -7/8 * Float.pi ..< -5/8 * Float.pi:
             direction = .UpperLeft
         case -5/8 * Float.pi ..< -3/8 * Float.pi:
-            direction = distOfTranslation > constants.farThreshold ? .FarLeft : .Left
+            direction = .Left
         case -3/8 * Float.pi ..< -1/8 * Float.pi:
             direction = .LowerLeft
         case -1/8 * Float.pi ..< 1/8 * Float.pi:
@@ -48,7 +53,7 @@ class keypad {
         case 1/8 * Float.pi ..< 3/8 * Float.pi:
             direction = .LowerRight
         case 3/8 * Float.pi ..< 5/8 * Float.pi:
-            direction = distOfTranslation > constants.farThreshold ? .FarRight : .Right
+            direction = .Right
         case 5/8 * Float.pi ..< 7/8 * Float.pi:
             direction = .UpperRight
         default:
@@ -59,5 +64,112 @@ class keypad {
     
     func dist(from point:CGPoint) -> CGFloat {
         return (point.x * point.x + point.y * point.y).squareRoot()
+    }
+    
+    func getKey(with direction: sweepDirection) -> keys? {
+        switch currentState {
+        case .selectKeypad:
+            switch direction {
+            case .Middle:
+                currentState = .selectedRTY
+            case .UpperLeft:
+                currentState = .selectedQWE
+            case .Up:
+                currentState = .selectedControlPad
+            case .UpperRight:
+                currentState = .selectedUIO
+            case .Left:
+                currentState = .selectedQWE
+            case .Right:
+                currentState = .selectedUIO
+            case .LowerLeft:
+                currentState = .selectedQWE
+            case .Down:
+                return .space
+            case .LowerRight:
+                currentState = .selectedUIO
+            }
+            return nil
+            
+        case .selectedQWE:
+            currentState = .selectKeypad
+            switch direction {
+            case .UpperLeft:
+                return capLocked ? .Q : .q
+            case .Up:
+                return capLocked ? .W : .w
+            case .UpperRight:
+                return capLocked ? .E : .e
+            case .Left:
+                return capLocked ? .A : .a
+            case .Middle:
+                return capLocked ? .S : .s
+            case .Right:
+                return capLocked ? .D : .d
+            case .LowerLeft:
+                return capLocked ? .Z : .z
+            case .Down:
+                return capLocked ? .X : .x
+            case .LowerRight:
+                return capLocked ? .C : .c
+            }
+        case .selectedRTY:
+            currentState = .selectKeypad
+            switch direction {
+            case .UpperLeft:
+                return capLocked ? .R : .r
+            case .Up:
+                return capLocked ? .T : .t
+            case .UpperRight:
+                return capLocked ? .Y : .y
+            case .Left:
+                return capLocked ? .F : .f
+            case .Middle:
+                return capLocked ? .G : .g
+            case .Right:
+                return capLocked ? .H : .h
+            case .LowerLeft:
+                return capLocked ? .V : .v
+            case .Down:
+                return capLocked ? .B : .b
+            case .LowerRight:
+                return capLocked ? .N : .n
+            }
+        case .selectedUIO:
+            currentState = .selectKeypad
+            switch direction {
+            case .UpperLeft:
+                return capLocked ? .U : .u
+            case .Up:
+                return capLocked ? .I : .i
+            case .UpperRight:
+                return capLocked ? .O : .o
+            case .Left:
+                return capLocked ? .J : .j
+            case .Middle:
+                return capLocked ? .K : .k
+            case .Right:
+                return capLocked ? .L : .l
+            case .LowerLeft:
+                return capLocked ? .M : .m
+            case .Down:
+                return nil
+            case .LowerRight:
+                return capLocked ? .P : .p
+            }
+        case .selectedControlPad:
+            currentState = .selectKeypad
+            switch direction {
+            case .Up:
+                return keys.capsLock
+            case .Left:
+                return keys.delete
+            case .Right:
+                return keys.enter
+            default:
+                return nil
+            }
+            
+        }
     }
 }
--- keyboardAlpha/ViewController.swift
+++ keyboardAlpha/ViewController.swift
@@ -1,20 +1,17 @@
-//
-//  ViewController.swift
-//  keyboardAlpha
-//
-//  Created by zzc on 9/16/18.
-//  Copyright © 2018 zzc. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var DisplayLabel: UILabel!
+    
+    @IBOutlet weak var inputField: UITextView!
+    @IBOutlet weak var controlPad: UILabel!
+    @IBOutlet weak var QWEPad: UILabel!
+    @IBOutlet weak var RTYPad: UILabel!
+    @IBOutlet weak var UIOPad: UILabel!
     @IBOutlet weak var keypad01: UILabel!
-    @IBOutlet weak var KeyboardView: UILabel!
-    @IBOutlet weak var curretInput: UILabel!
     var keypad01Model = keypad()
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,148 +23,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func keypad01TapHandler(_ sender: UITapGestureRecognizer) {
-        //DisplayLabel.text?.append("S")
-        DisplayLabel.text = "Middle";
-        if (flag == 0){
-            updateKeyboard()
-        }
-        else {
-            updateInput()
+        if let key = keypad01Model.getKey(with: .Middle) {
+            inputField.text.append(key.rawValue)
         }
     }
-    var flag = 0;
-    func updateKeyboard() {
-        
-        if (DisplayLabel.text == "Left") {
     
-            KeyboardView.text = "q w e\n a s d\n z x c"
-            flag = 1;
-            
-        }
-        else if(DisplayLabel.text == "Middle"){
-            KeyboardView.text = "r t y\nf g h\nv b n"
-            flag = 2;
-
-        }
-        else if(DisplayLabel.text == "Right"){
-            KeyboardView.text = "u i o\nj k l\nm    p"
-            flag = 3;
-          
-
-        }
-    }
-    func updateInput(){
-        //DisplayLabel.text = "Label"
-        
-        if (flag == 1){
-            if (DisplayLabel.text == "Left") {
-                curretInput.text = "a"
-            }
-            else if (DisplayLabel.text == "Middle") {
-                curretInput.text = "s"
-            }
-            else if (DisplayLabel.text == "Right") {
-                curretInput.text = "d"
-            }
-            else if (DisplayLabel.text == "UpperLeft") {
-                curretInput.text = "q"
-            }
-            else if (DisplayLabel.text == "Up") {
-                curretInput.text = "w"
-            }
-            else if (DisplayLabel.text == "UpperRight") {
-                curretInput.text = "e"
-            }
-            else if (DisplayLabel.text == "LowerLeft") {
-                curretInput.text = "z"
-            }
-            else if (DisplayLabel.text == "Down") {
-                curretInput.text = "x"
-            }
-            else if (DisplayLabel.text == "LowerRight") {
-                curretInput.text = "c"
-            }
-            
-        }
-        if (flag == 2){
-            if (DisplayLabel.text == "Left") {
-                curretInput.text = "f"
-            }
-            else if (DisplayLabel.text == "Middle") {
-                curretInput.text = "g"
-            }
-            else if (DisplayLabel.text == "Right") {
-                curretInput.text = "h"
-            }
-            else if (DisplayLabel.text == "UpperLeft") {
-                curretInput.text = "r"
-            }
-            else if (DisplayLabel.text == "Up") {
-                curretInput.text = "t"
-            }
-            else if (DisplayLabel.text == "UpperRight") {
-                curretInput.text = "y"
-            }
-            else if (DisplayLabel.text == "LowerLeft") {
-                curretInput.text = "v"
-            }
-            else if (DisplayLabel.text == "Down") {
-                curretInput.text = "b"
-            }
-            else if (DisplayLabel.text == "LowerRight") {
-                curretInput.text = "n"
-            }
-            
-            
-        }
-        if (flag == 3){
-            if (DisplayLabel.text == "Left") {
-                curretInput.text = "j"
-            }
-            else if (DisplayLabel.text == "Middle") {
-                curretInput.text = "k"
-            }
-            else if (DisplayLabel.text == "Right") {
-                curretInput.text = "l"
-            }
-            else if (DisplayLabel.text == "UpperLeft") {
-                curretInput.text = "u"
-            }
-            else if (DisplayLabel.text == "Up") {
-                curretInput.text = "i"
-            }
-            else if (DisplayLabel.text == "UpperRight") {
-                curretInput.text = "o"
-            }
-            else if (DisplayLabel.text == "LowerLeft") {
-                curretInput.text = "m"
-            }
-            else if (DisplayLabel.text == "Down") {
-                curretInput.text = " "
-            }
-            else if (DisplayLabel.text == "LowerRight") {
-                curretInput.text = "p"
-            }
-            
-            
-        }
-        
-        flag = 0;
-        KeyboardView.text = " "
-    }
     @IBAction func keypad01GestureHandler(_ sender: UIPanGestureRecognizer) {
         if sender.state == .ended {
-            DisplayLabel.text = "\(keypad01Model.direct(of: sender.translation(in: self.view)))"
-        }
-        if (flag == 0){
-            updateKeyboard()
-        }
-        else {
-            updateInput()
+            if let key = keypad01Model.getKey(with: keypad01Model.direct(of: sender.translation(in: keypad01))) {
+                switch key {
+                case .space:
+                    inputField.text.append(" ")
+                case .delete:
+                    inputField.text.removeLast()
+                case .capsLock:
+                    keypad01Model.capLocked.toggle()
+                case .enter:
+                    inputField.text.append("\n")
+                default:
+                    inputField.text.append(key.rawValue)
+                }
+            }
+            
+            //            DisplayLabel.text = "\(keypad01Model.direct(of: sender.translation(in: self.view)))"
         }
     }
-    
-    
     
     
 }
